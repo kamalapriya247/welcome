b920de865acf09e7485b10438dd31f61ef93556d
Kamalapriya247 <kamalapriya.kandaswamy@cesltd.com>
2017-06-29 20:08:54 +0530

    Initial Commit

---
+++ DotNETTraining/DotNETTraining/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNETTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int rev = 0;
+            int remainder;
+            int sum=0;
+           while(a!=0)
+           {
+               remainder = a % 10;
+               rev = rev * 10 + remainder;
+               a = a / 10;
+           }
+            while(a!=0)
+            {
+                remainder= a % 10;
+                sum = sum + remainder;
+                a = a / 10;
+            }
+        }
+    }
+}
